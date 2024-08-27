eb3c08da1a0fe75d6e420c2707f4d9caa64f465f
lucas lima <lucas.lima@suporterenova.onmicrosoft.com>
2024-08-27 15:06:15 -0300

    Revert "Updates"
    This reverts commit 03d3f6f21f4ca4d0101e9cec8a497fb1bf5e3acb.

---
+++ src/linhas/lat.jsx
@@ -0,0 +1,178 @@
+import React from "react";
+import "../index.css";
+
+const Lanthanides = () => {
+  return (
+    <div className="periodic-table">
+      {/* Lantânio */}
+      <div
+        className="element solid"
+        style={{ gridColumn: 4, gridRow: 9 }}
+        data-group="lanthanide"
+      >
+        <div className="atomic-number">57</div>
+        <div className="symbol">La</div>
+        <div className="name">Lantânio</div>
+        <div className="atomic-weight">(138,91)</div>
+      </div>
+
+      {/* Cério */}
+      <div
+        className="element solid"
+        style={{ gridColumn: 5, gridRow: 9 }}
+        data-group="lanthanide"
+      >
+        <div className="atomic-number">58</div>
+        <div className="symbol">Ce</div>
+        <div className="name">Cério</div>
+        <div className="atomic-weight">(140,12)</div>
+      </div>
+
+      {/* Praseodímio */}
+      <div
+        className="element solid"
+        style={{ gridColumn: 6, gridRow: 9 }}
+        data-group="lanthanide"
+      >
+        <div className="atomic-number">59</div>
+        <div className="symbol">Pr</div>
+        <div className="name">Praseodímio</div>
+        <div className="atomic-weight">(140,91)</div>
+      </div>
+
+      {/* Neodímio */}
+      <div
+        className="element solid"
+        style={{ gridColumn: 7, gridRow: 9 }}
+        data-group="lanthanide"
+      >
+        <div className="atomic-number">60</div>
+        <div className="symbol">Nd</div>
+        <div className="name">Neodímio</div>
+        <div className="atomic-weight">(144,24)</div>
+      </div>
+
+      {/* Promécio */}
+      <div
+        className="element solid"
+        style={{ gridColumn: 8, gridRow: 9 }}
+        data-group="lanthanide"
+      >
+        <div className="atomic-number">61</div>
+        <div className="symbol">Pm</div>
+        <div className="name">Promécio</div>
+        <div className="atomic-weight">(145)</div>
+      </div>
+
+      {/* Samário */}
+      <div
+        className="element solid"
+        style={{ gridColumn: 9, gridRow: 9 }}
+        data-group="lanthanide"
+      >
+        <div className="atomic-number">62</div>
+        <div className="symbol">Sm</div>
+        <div className="name">Samário</div>
+        <div className="atomic-weight">(150,36)</div>
+      </div>
+
+      {/* Europício */}
+      <div
+        className="element solid"
+        style={{ gridColumn: 10, gridRow: 9 }}
+        data-group="lanthanide"
+      >
+        <div className="atomic-number">63</div>
+        <div className="symbol">Eu</div>
+        <div className="name">Europício</div>
+        <div className="atomic-weight">(151,96)</div>
+      </div>
+
+      {/* Gadolínio */}
+      <div
+        className="element solid"
+        style={{ gridColumn: 11, gridRow: 9 }}
+        data-group="lanthanide"
+      >
+        <div className="atomic-number">64</div>
+        <div className="symbol">Gd</div>
+        <div className="name">Gadolínio</div>
+        <div className="atomic-weight">(157,25)</div>
+      </div>
+
+      {/* Térbio */}
+      <div
+        className="element solid"
+        style={{ gridColumn: 12, gridRow: 9 }}
+        data-group="lanthanide"
+      >
+        <div className="atomic-number">65</div>
+        <div className="symbol">Tb</div>
+        <div className="name">Térbio</div>
+        <div className="atomic-weight">(158,93)</div>
+      </div>
+
+      {/* Disprósio */}
+      <div
+        className="element solid"
+        style={{ gridColumn: 13, gridRow: 9 }}
+        data-group="lanthanide"
+      >
+        <div className="atomic-number">66</div>
+        <div className="symbol">Dy</div>
+        <div className="name">Disprósio</div>
+        <div className="atomic-weight">(162,50)</div>
+      </div>
+
+      {/* Hólmio */}
+      <div
+        className="element solid"
+        style={{ gridColumn: 14, gridRow: 9 }}
+        data-group="lanthanide"
+      >
+        <div className="atomic-number">67</div>
+        <div className="symbol">Ho</div>
+        <div className="name">Hólmio</div>
+        <div className="atomic-weight">(164,93)</div>
+      </div>
+
+      {/* Érbio */}
+      <div
+        className="element solid"
+        style={{ gridColumn: 15, gridRow: 9 }}
+        data-group="lanthanide"
+      >
+        <div className="atomic-number">68</div>
+        <div className="symbol">Er</div>
+        <div className="name">Érbio</div>
+        <div className="atomic-weight">(167,26)</div>
+      </div>
+
+      {/* Túlio */}
+      <div
+        className="element solid"
+        style={{ gridColumn: 16, gridRow: 9 }}
+        data-group="lanthanide"
+      >
+        <div className="atomic-number">69</div>
+        <div className="symbol">Tm</div>
+        <div className="name">Túlio</div>
+        <div className="atomic-weight">(168,93)</div>
+      </div>
+
+      {/* Lutécio */}
+      <div
+        className="element solid"
+        style={{ gridColumn: 17, gridRow: 9 }}
+        data-group="lanthanide"
+      >
+        <div className="atomic-number">70</div>
+        <div className="symbol">Lu</div>
+        <div className="name">Lutécio</div>
+        <div className="atomic-weight">(174,97)</div>
+      </div>
+    </div>
+  );
+};
+
+export default Lanthanides;
